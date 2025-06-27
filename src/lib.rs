98f618f778c4354c53b63b0bdd85495274a6ce25
Abhinav Tripahti <satsyndicate@gmail.com>
2025-06-27 11:40:45 +0000

    Implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,64 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//implementing the rc type
+
+use std::cell::Cell;
+pub mod Rc {
+struct RcInner<T> {
+    value: T,
+    count: Cell<usize>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Rc<T> {
+    val: *mut RcInner<T>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl <T> Rc <T> {
+    fn new(val: T) -> Self {
+        let box = Box::new(RcInner{
+            value : T,
+            count : Cell::new(1),
+        });
+        Rc {
+            val : Box::into_raw(box),
+        }
     }
+}
+
+impl<T> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+        let a = unsafe {&*self.val};
+        let n = a.count.get();
+        a.count.set(a+1);
+        Rc {
+            val : self.val;
+        }
+    }
+}
+
+impl <T> std::ops::Deref for Rc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe {
+            &(&*self.val).value
+        }
+    }
+}
+
+impl <T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        let a = unsafe { &*self.val };
+        let b = a.count.get();
+        if b == 1 {
+            //we are the last reference and thus we have to deallocate the heap allocated box 
+            drop(a);
+            let _ = Box::from_raw(self.val);     
+            /* box_raw does not take a raw const pointer as input
+            instead it takes a raw mut pointer as input..we could still use val if it is a raw const
+            pointer by using the concept og nonull provided by the ptr module of the standard library  */
+        }
+        else {
+            //decrement the count by 1 that is..
+            a.count.set(b-1);
+        }
+    }
+}
 }
